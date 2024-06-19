d71696f5898144e12323be3ef0e636adb17c7e1c
Zynalex9 <alzn952@gmail.com>
2024-06-19 12:32:51 +0500

    Page 2 added

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-});
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("#main"),
+//   smooth: true,
+// });
 const elems = document.querySelectorAll("#page2 .elem");
+var tl = gsap.timeline();
+tl.from("#main nav h3", {
+  opacity: 0,
+  duration: 0.5,
+  y: -100,
+});
+tl.from("#main #page1 .headings h1", {
+  opacity: 0,
+  duration: 0.5,
+  y: 100,
+  stagger: 0.3,
+});
+tl.from("#main #page1 .headings h3", {
+  opacity: 0,
+  duration: 0.4,
+  x: -100,
+});
+tl.from("#main #page1 .footer-headings h3", {
+  opacity: 0,
+  duration: 0.5,
+  y: -100,
+});
 elems.forEach((elem) => {
   elem.addEventListener("mouseleave", () => {
     gsap.to(elem.querySelector("img"), {
       opacity: 0,
       duration: 1,
+    });
+    gsap.to(elem.querySelector("h1"), {
+        opacity:1,
+        duration:0.6
     });
   });
   elem.addEventListener("mousemove", (e) => {
@@ -18,6 +44,10 @@ elems.forEach((elem) => {
       top: elemTop,
       left: e.clientX,
       ease: Power3,
+    });
+    gsap.to(elem.querySelector("h1"), {
+        opacity:0.6,
+        duration:0.6
     });
   });
 });
